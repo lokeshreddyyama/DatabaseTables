518cee0e417ac243c7022563bedbfda7da61c679
Your Name <you@example.com>
2026-01-28 18:11:19 +0530

    fs

---
+++ Course.sql
@@ -0,0 +1,5 @@
+INSERT INTO `` (`Course Code`,`Course Name`,`Faculty Id`,`Student Id`,`Faculty Email`) VALUES ('2301cs101','JAVA','Fac013234','vtu002','fac01@gmail.com');
+INSERT INTO `` (`Course Code`,`Course Name`,`Faculty Id`,`Student Id`,`Faculty Email`) VALUES ('2301cs102','HTML','Fac013561','vtu003','fac02@gmail.com');
+INSERT INTO `` (`Course Code`,`Course Name`,`Faculty Id`,`Student Id`,`Faculty Email`) VALUES ('2301cs202','CSS','Fac013202','vtu004','fac03@gmail.com');
+INSERT INTO `` (`Course Code`,`Course Name`,`Faculty Id`,`Student Id`,`Faculty Email`) VALUES ('2301cc201','JAVASCRIPT','Fac013356','vtu005','fac04@gmail.com');
+INSERT INTO `` (`Course Code`,`Course Name`,`Faculty Id`,`Student Id`,`Faculty Email`) VALUES ('2301cc203','BOOTSTRAP','Fac013356','vtu006','fac05@gmail.com');
---
+++ Student.sql
@@ -0,0 +1,5 @@
+INSERT INTO `` (`vtu no`,`Name`,`Email`,`Phone No`,`Department`) VALUES ('25360','YAMA LOKESH REDDY','lokeshreddy@gmail.com','9158356825','CSE(CS)');
+INSERT INTO `` (`vtu no`,`Name`,`Email`,`Phone No`,`Department`) VALUES ('26309','PANDU','pandu@123gmail.com','9425355545','CSE');
+INSERT INTO `` (`vtu no`,`Name`,`Email`,`Phone No`,`Department`) VALUES ('24594','Harish','harish12345@gmail.com','9458526655','AIML');
+INSERT INTO `` (`vtu no`,`Name`,`Email`,`Phone No`,`Department`) VALUES ('26548','MANJU','manju234@gmail.com','9896525655','ECE');
+INSERT INTO `` (`vtu no`,`Name`,`Email`,`Phone No`,`Department`) VALUES ('23565','bittu','bittu@735gmail.com','9566436686','CSE(DS)');
